63d2e5fe84065eada01bdfbb283ff8c0dc7f5bbb
OneBearr <56367478+OneBearr@users.noreply.github.com>
2022-10-16 19:18:32 -0700

    Update and rename sysbench_fileio_1.sh to sysbench_fileio_3.sh

--- HW1/sysbench_fileio_3.sh
+++ HW1/sysbench_fileio_3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "sysbench File IO experiment 1: with 16 Threads and total file size as 3GB, combined random read/write"
+echo "sysbench File IO experiment 3: with 16 Threads and total file size as 3GB, combined random read/write"
 for((i = 0; i < 5; i++))
 do
       sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndrw prepare
