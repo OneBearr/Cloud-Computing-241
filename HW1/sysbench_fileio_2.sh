550efb8ca78c5304841c84f5c5f6ebd6cc012ef6
OneBearr <56367478+OneBearr@users.noreply.github.com>
2022-10-17 00:30:26 -0700

    Update sysbench_fileio_2.sh

--- HW1/sysbench_fileio_2.sh
+++ HW1/sysbench_fileio_2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "sysbench File IO experiment 2: with 16 Threads and total file size as 3GB, random write"
+echo "sysbench File IO experiment 2: with 16 Threads and total file size as 1GB, random write"
 for((i = 0; i < 5; i++))
 do
-      sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndwr prepare
-      sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndwr run
-      sysbench --num-threads=16 --test=fileio --file-total-size=3G --file-test-mode=rndwr cleanup
+      sysbench --num-threads=16 --test=fileio --file-total-size=1G --file-test-mode=rndwr prepare
+      sysbench --num-threads=16 --test=fileio --file-total-size=1G --file-test-mode=rndwr run
+      sysbench --num-threads=16 --test=fileio --file-total-size=1G --file-test-mode=rndwr cleanup
 done
